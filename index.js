17f3548db7ff24242e0b7241f20e317e7cc09237
liyanfang <1136315144@qq.com>
2023-04-12 22:48:45 +0800

    chore: update git infomation

---
+++ index.js
@@ -0,0 +1,5 @@
+import components from './src/index'
+export * from './src/index'
+export default {
+  install: (app) => components.forEach((component) => app.use(component))
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,46 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 8899
+  },
+  base: './',
+  build: {
+    //
+    target: 'modules',
+    // 打包文件目录
+    outDir: 'es',
+    // 压缩
+    minify: true,
+    rollupOptions: {
+      // 忽略打包vue, element-plus
+      external: ['vue', 'element-plus'],
+      input: ['index.js'],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          exports: 'named',
+          dir: resolve(__dirname, './dist/es')
+        },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          exports: 'named',
+          dir: resolve(__dirname, './dist/lib')
+        }
+      ]
+    },
+    lib: {
+      entry: './index.js',
+      name: 'lvyb',
+      formats: ['es', 'cjs']
+    }
   }
 })
